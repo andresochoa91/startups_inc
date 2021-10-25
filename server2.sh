e4474e20adaec9ed51a900febc938f3b887619b6
andresochoa91 <andres_ochoa91@hotmail.com>
2021-10-24 17:43:45 -0700

    Changes in sh files

--- server2.sh
+++ server2.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-function startups_inc () {
-  echo "What's your MySQL DB user?"
-  read user
+function startups_inc () { 
   if [ $1 = "db" ]
   then
-    mysql.server start 
+    echo "What's your MySQL DB user?"
+    read user
     mysql -u $user -p --database=use startups_inc
   elif [ $1 = "dump_db" ]
   then
+    echo "What's your MySQL DB user?"
+    read user
     mysqldump -u $user -p startups_inc > startups_inc.sql
   fi
 }
